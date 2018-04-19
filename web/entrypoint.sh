5ef0478b17c7ced87068d7fdfb08ff68acf78e39
Julio Felipe Angelini <julio.angelini@tapps.com.br>
2018-04-19 19:27:48 -0300

    Version 1.0.0

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
-if [ "$@" = "nginx -g daemon off;" ]; then
+args="$*"
+
+if [ "$args" = 'nginx -g daemon off;' ]; then
     # Linking non-ssl nginx configuration file
     ln -s /etc/nginx/sites-available/mattermost /etc/nginx/conf.d/mattermost.conf
 
@@ -9,28 +11,32 @@ if [ "$@" = "nginx -g daemon off;" ]; then
     sed -i "s/{%APP_HOST%}/app/g" /etc/nginx/conf.d/mattermost.conf
     sed -i "s/{%APP_PORT%}/80/g" /etc/nginx/conf.d/mattermost.conf
 
+    (
+        sleep 10
+
+        # Run certbot certonly.
+        # If your volume mounts are set up correctly this should only spend an ACME challenge
+        # when no valid certificates are found in "/etc/letsencrypt/live/${APP_DOMAIN}"
+        certbot -n -m REPLACE_WITH_EMAIL --agree-tos -d "${APP_DOMAIN}" --nginx certonly
+
+        # Linking ssl nginx configuration file
+        rm /etc/nginx/conf.d/mattermost.conf
+        ln -s /etc/nginx/sites-available/mattermost-ssl /etc/nginx/conf.d/mattermost.conf
+
+        # Setup app host, port and domain on configuration file
+        sed -i "s/{%APP_HOST%}/app/g" /etc/nginx/conf.d/mattermost.conf
+        sed -i "s/{%APP_PORT%}/80/g" /etc/nginx/conf.d/mattermost.conf
+        sed -i "s/{%APP_DOMAIN%}/${APP_DOMAIN}/g" /etc/nginx/conf.d/mattermost.conf
+
+        # Reload nginx
+        echo "Reloading nginx" >> /proc/1/fd/1
+        /etc/init.d/nginx configtest && /etc/init.d/nginx reload
+    )&
+
     # Run Nginx
-    echo "Starting nginx with '$@' for pre-certbot setup" >> /proc/1/fd/1
+    echo "Starting nginx with '$args' for pre-certbot setup" >> /proc/1/fd/1
     exec "$@"
-
-    # Run certbot certonly.
-    # If your volume mounts are set up correctly this should only spend an ACME challenge
-    # when no valid certificates are found in "/etc/letsencrypt/live/${APP_DOMAIN}"
-    certbot -n -m julio.angelini@gmail.com --agree-tos -d "${APP_DOMAIN}" --nginx certonly
-
-    # Linking ssl nginx configuration file
-    rm /etc/nginx/conf.d/mattermost.conf
-    ln -s /etc/nginx/sites-available/mattermost-ssl /etc/nginx/conf.d/mattermost.conf
-
-    # Setup app host, port and domain on configuration file
-    sed -i "s/{%APP_HOST%}/app/g" /etc/nginx/conf.d/mattermost.conf
-    sed -i "s/{%APP_PORT%}/80/g" /etc/nginx/conf.d/mattermost.conf
-    sed -i "s/{%APP_HOST%}/${APP_DOMAIN}/g" /etc/nginx/conf.d/mattermost.conf
-
-    # Run Nginx
-    echo "Reloading nginx" >> /proc/1/fd/1
-    exec "/etc/init.d/nginx configtest && /etc/init.d/nginx reload"
 else
-    echo "Running custom command '$@'" >> /proc/1/fd/1
+    echo "Running custom command '$args'" >> /proc/1/fd/1
     exec "$@"
 fi
